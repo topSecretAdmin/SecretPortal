955d3fd7e6bec5c8939ec01548f201155a66b14a
topSecretAdmin <otk_protonmail@protonmail.com>
2023-12-25 14:21:25 +0200

    [ADD]inn module

---
+++ inn/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Inn',
+    'version': '1.0.0',
+    'summary': 'Inn Customization',
+    'description': """",
+    Long Description of your modules""",
+    'author': 'Lorn',
+    'company': 'Zrobleno Solutions',
+    'website': 'https://www.odoo.com',
+    'depends': ['crm'],
+    'data': [
+        'views/inn_view.xml'
+    ],
+    'license': 'LGPL-3',
+}
---
+++ inn/models/inn.py
@@ -0,0 +1,26 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import requests
+
+class InfoBaza(models.Model):
+    _inherit = 'crm.lead'
+
+    info_baza = fields.Char(string='InfoBazaS', index=True)
+
+    @api.constrains('info_baza')
+    def _check_info_baza(self):
+        for record in self:
+            if record.info_baza:
+                if not record.info_baza.isdigit():
+                    raise ValidationError('ІПН має складатись з цифр')
+                if len(record.info_baza) != 10:
+                    raise ValidationError('іПН має складатись з 10 цифр')
+
+    def action_send_inn(self):
+        inn_value = self.info_baza
+        lead_id = self.id
+        api_url = 'http://localhost:5001/user'
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Catch-Control': 'no-cache'}
+        payload = {'lead_id': lead_id, 'inn_value': inn_value}
+
+        return requests.post(api_url, headers=headers, json=payload)
