6121419e50b91e8d7564f55c922f8bf7a8288e3b
topSecretAdmin <otk_protonmail@protonmail.com>
2023-12-27 14:28:58 +0200

    [Update]url

--- inn/models/inn.py
+++ inn/models/inn.py
@@ -20,7 +20,7 @@ class InfoBaza(models.Model):
     def action_send_inn(self):
         inn_value = self.info_baza
         lead_id = self.id
-        api_url = 'https://localhost:5001/user'
+        api_url = 'http://192.168.0.101:5001/user'
         headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Catch-Control': 'no-cache'}
         payload = {'lead_id': lead_id, 'inn_value': inn_value}
 
