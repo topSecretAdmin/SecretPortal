be86f06914176a0096b81769ccf1bb7c75b51bd5
topSecretAdmin <otk_protonmail@protonmail.com>
2024-01-04 00:56:27 +0200

    [UPDATE]filed position

--- inn/__manifest__.py
+++ inn/__manifest__.py
@@ -7,7 +7,8 @@
     'author': 'Lorn',
     'company': 'Zrobleno Solutions',
     'website': 'https://www.odoo.com',
-    'depends': ['crm'],
+    'depends': ['crm', 'studio_customization', 'web_studio'],
+    # 'web_studio'
     'data': [
         'views/inn_view.xml'
     ],
